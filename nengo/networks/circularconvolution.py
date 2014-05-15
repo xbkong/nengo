1661b5eab1809fb2a14dd4737e1bcd3cac9c0b7b
Eric Crawford <e2crawfo@uwaterloo.ca>
2014-05-15 18:01:46 -0400

    Added **ens_kwargs to __init__ of circconv.
    Lets one specify the properties of the ensembles making up the Product
    network inside the CircularConvolution network. Importantly, lets one
    specify the radius.

--- nengo/networks/circularconvolution.py
+++ nengo/networks/circularconvolution.py
@@ -83,15 +83,18 @@ class CircularConvolution(nengo.Network):
     only the real part of `c` since the imaginary part is analytically zero.
     """
 
-    def __init__(self, n_neurons, dimensions, invert_a=False, invert_b=False):
+    def __init__(self, n_neurons, dimensions, invert_a=False, invert_b=False,
+                 **ens_kwargs):
         self.dimensions = dimensions
         self.invert_a = invert_a
         self.invert_b = invert_b
 
         self.A = nengo.Node(size_in=dimensions, label="A")
         self.B = nengo.Node(size_in=dimensions, label="B")
-        self.product = Product(
-            n_neurons, self.transform_out.shape[1], label="conv")
+        self.product = Product(n_neurons,
+                               self.transform_out.shape[1],
+                               label="conv",
+                               **ens_kwargs)
         self.output = nengo.Node(size_in=dimensions, label="output")
 
         nengo.Connection(self.A, self.product.A,
