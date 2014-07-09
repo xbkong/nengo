cd56c2df4aa9dcd4ec8ed348cde173c94dd387df
Jan Gosmann <jgosmann@uwaterloo.ca>
2014-07-09 12:47:26 -0400

    Implement DotProduct in terms of Product.

--- nengo/spaopt/dotproduct.py
+++ nengo/spaopt/dotproduct.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 import nengo
-from nengo.spaopt.unitea import UnitEA
+from nengo.spaopt.product import Product
 
 
 class DotProduct(nengo.Network):
@@ -12,22 +12,18 @@ class DotProduct(nengo.Network):
 
     def __init__(self, n_neurons, dimensions, radius=1.0, eval_points=None,
                  **ens_kwargs):
-        self.config[nengo.Ensemble].update(ens_kwargs)
         self.A = nengo.Node(size_in=dimensions, label="A")
         self.B = nengo.Node(size_in=dimensions, label="B")
         self.output = nengo.Node(size_in=1, label="output")
         self.dimensions = dimensions
         self.radius = radius
 
-        self.product = UnitEA(
-            n_neurons, dimensions, dimensions, 2,
-            radius=radius, eval_points=eval_points)
+        self.product = Product(
+            n_neurons, dimensions, radius, eval_points, **ens_kwargs)
+
+        nengo.Connection(self.A, self.product.A, synapse=None)
+        nengo.Connection(self.B, self.product.B, synapse=None)
 
         nengo.Connection(
-            self.A, self.product.input[::2], synapse=None)
-        nengo.Connection(
-            self.B, self.product.input[1::2], synapse=None)
-
-        nengo.Connection(
-            self.product.add_output('product', lambda x: x[0] * x[1]),
-            self.output, synapse=None, transform=[dimensions * [1.0]])
+            self.product.output, self.output, synapse=None,
+            transform=self.product.dot_product_transform())
