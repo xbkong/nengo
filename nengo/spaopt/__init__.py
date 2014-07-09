db9a3aa491ce71214d31d4c353d115f944f4884d
Jan Gosmann <jgosmann@uwaterloo.ca>
2014-07-09 15:58:39 -0400

    Use BasalGanglia and Thalamus from spa in spaopt.

--- nengo/spaopt/__init__.py
+++ nengo/spaopt/__init__.py
@@ -1,5 +1,6 @@
 from ..spa import SPA
 from ..spa.actions import Actions
+from ..spa.basalganglia import BasalGanglia  # TODO
 from .buffer import Buffer
 from .compare import Compare
 from .circularconvolution import CircularConvolution
@@ -10,5 +11,7 @@ from ..spa.memory import Memory
 from ..spa.module import Module
 from .product import Product
 from ..spa.utils import similarity
+#from .thalamus import Thalamus  # FIXME
+from ..spa.thalamus import Thalamus
 from .unitea import UnitEA
 from ..spa.vocab import Vocabulary
