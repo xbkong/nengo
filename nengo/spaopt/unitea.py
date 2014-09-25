52e3aee4fe59b00f8dcc275f8670eb60c2677811
Jan Gosmann <jgosmann@uwaterloo.ca>
2014-09-25 15:31:50 -0400

    Update spaopt to new radius optimizations.

--- nengo/spaopt/unitea.py
+++ nengo/spaopt/unitea.py
@@ -4,30 +4,23 @@ import nengo
 from nengo.networks.ensemblearray import EnsembleArray
 from nengo.params import Default
 from nengo.utils.compat import is_number
-from nengo.utils.optimization import sp_subvector_optimal_radius
+from nengo.utils.optimization import SubvectorRadiusOptimizer
 
 
 # TODO unittest
 class UnitEA(EnsembleArray):
 
     def __init__(self, n_neurons, dimensions, n_ensembles, ens_dimensions=1,
-                 label=None, radius=1.0, n_eval_points=Default,
-                 eval_points=Default, **ens_kwargs):
+                 label=None, radius=1.0, **ens_kwargs):
         if dimensions % n_ensembles != 0:
             raise ValueError(
                 "'dimensions' has to be divisible by 'n_ensembles'.")
 
-        if eval_points is Default:
-            if n_eval_points is Default:
-                n_eval_points = max(
-                    np.clip(500 * ens_dimensions, 750, 2500), 2 * n_neurons)
-
-        scaled_r = radius * sp_subvector_optimal_radius(
-            dimensions, dimensions // n_ensembles, ens_dimensions,
-            n_eval_points if n_eval_points is not Default
-            else len(eval_points))
+        optimizer = SubvectorRadiusOptimizer(
+            n_neurons, ens_dimensions, **ens_kwargs)
+        scaled_r = radius * optimizer.find_optimal_radius(
+            dimensions, dimensions // n_ensembles)
 
         super(UnitEA, self).__init__(
             n_neurons, n_ensembles, ens_dimensions, label=label,
-            radius=scaled_r, n_eval_points=n_eval_points,
-            eval_points=eval_points, **ens_kwargs)
+            radius=scaled_r, **ens_kwargs)
