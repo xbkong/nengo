c0ef450a7cbca88e2bfc1e9defdfac122ebde46f
Jan Gosmann <jgosmann@uwaterloo.ca>
2014-11-28 16:35:35 -0500

    Update optimized product network.
    This removes the special sampling of evaluation points. This should
    be reintroduced.

--- nengo/spaopt/product.py
+++ nengo/spaopt/product.py
@@ -3,6 +3,7 @@ import numpy as np
 import nengo
 from nengo.networks.ensemblearray import EnsembleArray
 from nengo.utils.compat import is_number
+from nengo.utils.distributions import Choice
 from nengo.utils.optimization import SubvectorRadiusOptimizer
 
 
@@ -15,49 +16,30 @@ class Product(nengo.Network):
 
     def __init__(
             self, n_neurons, dimensions, radius=1.0,
-            n_eval_points=nengo.Default, eval_points=nengo.Default,
             encoders=nengo.Default, **ens_kwargs):
+        self.config[nengo.Ensemble].update(ens_kwargs)
         self.A = nengo.Node(size_in=dimensions, label="A")
         self.B = nengo.Node(size_in=dimensions, label="B")
-        self.output = nengo.Node(size_in=dimensions, label="output")
         self.dimensions = dimensions
-        self.radius = radius
-
-        if n_eval_points is nengo.Default:
-            try:
-                n_eval_points = len(eval_points)
-            except (AttributeError, TypeError):
-                n_eval_points = max(1000, 2 * n_neurons)
 
         if encoders is nengo.Default:
-            encoders = np.tile(
-                [[1, 1], [1, -1], [-1, 1], [-1, -1]],
-                ((n_neurons // 4) + 1, 1))[:n_neurons]
+            encoders = Choice([[1, 1], [1, -1], [-1, 1], [-1, -1]])
 
         optimizer = SubvectorRadiusOptimizer(n_neurons, 2, **ens_kwargs)
         scaled_r = radius * optimizer.find_optimal_radius(dimensions, 1)
 
-        if is_number(eval_points):
-            xs = np.linspace(-scaled_r, scaled_r, int(np.sqrt(n_eval_points)))
-            xs, ys = np.meshgrid(xs, xs)
-            eval_points = np.vstack((xs.flat, ys.flat)).T
-            eval_points = np.dot(np.array(
-                [[np.cos(np.pi/4), -np.sin(np.pi/4)],
-                 [np.sin(np.pi/4), np.cos(np.pi/4)]]), eval_points.T).T
+        # FIXME use square evaluation points
 
         self.product = EnsembleArray(
             n_neurons, n_ensembles=dimensions, ens_dimensions=2,
-            radius=scaled_r, encoders=encoders, n_eval_points=n_eval_points,
-            eval_points=eval_points, **ens_kwargs)
+            radius=scaled_r, encoders=encoders, **ens_kwargs)
 
         nengo.Connection(
             self.A, self.product.input[::2], synapse=None)
         nengo.Connection(
             self.B, self.product.input[1::2], synapse=None)
 
-        nengo.Connection(
-            self.product.add_output('product', lambda x: x[0] * x[1]),
-            self.output, synapse=None)
+        self.output = self.product.add_output('product', lambda x: x[0] * x[1])
 
     def dot_product_transform(self, scale=1.0):
         """Returns a transform for output to compute the scaled dot product."""
