f2b68ce384e8f61c8523db0c654b292d82e47d38
drasmuss <drasmuss@uwaterloo.ca>
2015-03-18 14:55:45 -0400

    Remove SignalView from signal logic
    Joint commit with Eric Hunsberger <erichuns@gmail.com>.

--- nengo/utils/numpy.py
+++ nengo/utils/numpy.py
@@ -39,6 +39,28 @@ def array(x, dims=None, min_dims=0, readonly=False, **kwargs):
     return y
 
 
+def array_base(x):
+    """Get base array (that is NOT a view) for x.
+
+    In Numpy >= 1.7, this is simply ``x.base``. However, in Numpy <= 1.6,
+    we need to loop back through the bases, since bases can be views.
+    """
+    while x.base is not None:
+        x = x.base
+    return x
+
+
+def array_offset(x):
+    """Get offset of array data from base data in bytes."""
+    if x.base is None:
+        return 0
+
+    base = array_base(x)
+    base_start = base.__array_interface__['data'][0]
+    start = x.__array_interface__['data'][0]
+    return start - base_start
+
+
 def expm(A, n_factors=None, normalize=False):
     """Simple matrix exponential to replace Scipy's matrix exponential
 
