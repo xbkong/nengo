eb726ae2750ba19ee6156a9b26f058baf471f718
Terry Stewart <tcstewar@uwaterloo.ca>
2015-06-19 12:24:47 -0400

    Use ensemble to gate wm (fix for nengo_spinnaker)

--- nengo/networks/workingmemory.py
+++ nengo/networks/workingmemory.py
@@ -21,13 +21,13 @@ def InputGatedMemory(n_neurons, dimensions, fdbk_scale=1.0, gate_gain=10,
         # integrator to store value
         with mem_config:
             net.mem = EnsembleArray(n_neurons, dimensions,
-                                    neuron_nodes=True, label="mem")
+                                    neuron_nodes=False, label="mem")
             nengo.Connection(net.mem.output, net.mem.input,
                              transform=fdbk_scale)
 
         # calculate difference between stored value and input
         net.diff = EnsembleArray(n_neurons, dimensions,
-                                 neuron_nodes=True, label="diff")
+                                 neuron_nodes=False, label="diff")
         nengo.Connection(net.mem.output, net.diff.input, transform=-1)
 
         # feed difference into integrator
@@ -37,16 +37,27 @@ def InputGatedMemory(n_neurons, dimensions, fdbk_scale=1.0, gate_gain=10,
 
         # gate difference (if gate==0, update stored value,
         # otherwise retain stored value)
-        net.gate = nengo.Node(size_in=1)
-        nengo.Connection(net.gate, net.diff.neuron_input,
-                         transform=np.ones((n_total_neurons, 1)) * -gate_gain,
-                         synapse=None)
+        net.gate = nengo.Ensemble(50, 1, encoders=nengo.dists.Choice([[1]]),
+                                    intercepts=nengo.dists.Uniform(0.4, 0.9))
+        #net.gate = nengo.Node(size_in=1)
+        for ens in net.diff.all_ensembles:
+            nengo.Connection(net.gate, ens.neurons,
+                             transform=np.ones((ens.n_neurons, 1)) * -gate_gain,
+                             synapse=0.005)
+        #nengo.Connection(net.gate, net.diff.neuron_input,
+        #                 transform=np.ones((n_total_neurons, 1)) * -gate_gain,
+        #                 synapse=None)
 
         # reset input (if reset=1, remove all values, and set to 0)
-        net.reset = nengo.Node(size_in=1)
-        nengo.Connection(net.reset, net.mem.neuron_input,
-                         transform=np.ones((n_total_neurons, 1)) * -reset_gain,
-                         synapse=None)
+        net.reset = nengo.Ensemble(50, 1, encoders=nengo.dists.Choice([[1]]),
+                                    intercepts=nengo.dists.Uniform(0.4, 0.9))
+        #nengo.Connection(net.reset, net.mem.neuron_input,
+        #                 transform=np.ones((n_total_neurons, 1)) * -reset_gain,
+        #                 synapse=None)
+        for ens in net.mem.all_ensembles:
+            nengo.Connection(net.reset, ens.neurons,
+                             transform=np.ones((ens.n_neurons, 1)) * -reset_gain,
+                             synapse=0.005)
 
     net.input = net.diff.input
     net.output = net.mem.output
