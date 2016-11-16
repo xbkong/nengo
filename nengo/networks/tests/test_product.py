3f17f21d8e9f188ef3ef8974ae8ec434c61e13c7
Jan Gosmann <jgosmann@uwaterloo.ca>
2016-11-16 16:34:59 -0500

    Make old network inputs available as deprecated.
    This commit converts the Product and CircularConvolution network
    functions into classes to be able to add properties for the old inputs
    that give DeprecationWarnings. Because with the class we are not able
    to handle the net argument properly (except with some magic metaclass
    stuff that probably is not a good idea), it is made obsolete. Network
    arguments like label and seed can now passed as keyword arguments.
    
    I decided for the kwargs approach instead of listing the explicit
    arguments because:
    
    * less boilerplate
    * the relevant arguments seed and label are easy to remember (so users
      won't have to go back to the Network documentation a lot)
    * the argument add_to_container was for me always
      a hassle to remember when subclassing and isn't that relevant to the
      average user
    * kwargs enforces to use keyword arguments when instantiating the
      network.
    * It is a pretty common idiom (e.g. used in Matplotlib)
    
    I like that this simplified the instantiation in a few tests that
    actually were using the net argument.
    
    Addresses #887.

--- nengo/networks/tests/test_product.py
+++ nengo/networks/tests/test_product.py
@@ -10,8 +10,7 @@ from nengo.utils.numpy import rmse, maxint
 def test_sine_waves(Simulator, plt, seed):
     radius = 2
     dim = 5
-    product = nengo.networks.Product(
-        200, dim, radius, net=nengo.Network(seed=seed))
+    product = nengo.networks.Product(200, dim, radius, seed=seed)
 
     func_a = lambda t: np.sqrt(radius)*np.sin(np.arange(1, dim+1)*2*np.pi*t)
     func_b = lambda t: np.sqrt(radius)*np.sin(np.arange(dim, 0, -1)*2*np.pi*t)
@@ -47,8 +46,7 @@ def test_direct_mode_with_single_neuron(Simulator, plt, seed):
     config = nengo.Config(nengo.Ensemble)
     config[nengo.Ensemble].neuron_type = nengo.Direct()
     with config:
-        product = nengo.networks.Product(
-            1, dim, radius, net=nengo.Network(seed=seed))
+        product = nengo.networks.Product(1, dim, radius, seed=seed)
 
     func_a = lambda t: np.sqrt(radius)*np.sin(np.arange(1, dim+1)*2*np.pi*t)
     func_b = lambda t: np.sqrt(radius)*np.sin(np.arange(dim, 0, -1)*2*np.pi*t)
