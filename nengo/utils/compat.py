d8ac6e30c00ca0b7f8072aab07cbfd100ce23e5d
Sean Aubin <seanaubin@gmail.com>
2017-04-25 18:14:17 +0900

    fixup! ignore pylint unused-import

--- nengo/utils/compat.py
+++ nengo/utils/compat.py
@@ -88,7 +88,7 @@ if PY2:
 else:
     import pickle
     import configparser
-    from inspect import getfullargspec  # noqa
+    from inspect import getfullargspec  # pylint: disable=unused-import
     from io import StringIO
     from os import replace
     TextIO = StringIO
